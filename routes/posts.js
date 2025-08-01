972de04e708ef80fc3d8fb2d38935a81dcaaa8c8
dpalexandru <alexpoian91@hotmail.com>
2025-08-01 16:18:52 +0200

    feat: add routes for update and delete posts

--- routes/posts.js
+++ routes/posts.js
@@ -13,9 +13,23 @@ router.get("/:id", (req, res) => {
 
 //definisco la route per Creare un post
 router.post("/", (req, res) => {
-    res.send(`Sezione creazione post`)
+    res.send(`Sezione creazione nuovo post`)
 });
 
+//definisco la route per Modificare totale un post
+router.put("/:id", (req, res) => {
+    res.send(`Modifica totale del post n: ${req.params.id}`)
+});
+
+//definisco la route per Modificare parziale di un post
+router.patch("/:id", (req, res) => {
+    res.send(`Modifica parziale del post n: ${req.params.id}`)
+});
+
+//definisco la route per Eliminare  di un post
+router.delete("/:id", (req, res) => {
+    res.send(`Cancellazione del post: ${req.params.id}`)
+});
 
 //esporto le route
 module.exports = router;
