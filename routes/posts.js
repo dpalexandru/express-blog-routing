77782b11949cc7156160419f2bc214d808cb150f
dpalexandru <alexpoian91@hotmail.com>
2025-08-01 16:11:57 +0200

    feat: router to post id and post to create

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-//definisco la route principale
-
+//definisco la route principale Index
 router.get("/", (req, res) => {
     res.send("Lista dei miei post")
 });
+
+//definisco la route principale con id
+router.get("/:id", (req, res) => {
+    res.send(`Dettagli postn n: ${req.params.id}`)
+});
+
+//definisco la route per Creare un post
+router.post("/", (req, res) => {
+    res.send(`Sezione creazione post`)
+});
+
 
 //esporto le route
 module.exports = router;
