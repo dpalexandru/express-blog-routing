7858c12a42a88b6efc4f41cebcc93636b2587a9e
dpalexandru <alexpoian91@hotmail.com>
2025-08-01 15:30:16 +0200

    feat: add gitignore and personal scripts

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+//creo la rott principale 
+app.get("/", (req, res) => {
+    res.send("Benvenuto, collegamento riuscito")
+})
+
+//lascio il server in ascolto sulla porto stabilit
+app.listen(port, () => {
+    console.log("richiesta effettuata")
+})
