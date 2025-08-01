00bc1c5c4f6eb2198ef304652beba1f6a90a7787
dpalexandru <alexpoian91@hotmail.com>
2025-08-01 16:04:14 +0200

    feat: import routes posts in app.js

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//importo le routes 
+const postRoutes = require('./routes/posts.js')
+
 //creo la rotts principale 
 app.get("/", (req, res) => {
     res.send("Benvenuto, collegamento riuscito")
-})
+});
+
+//autorizzo all'uso delle routes a posts
+app.use('/posts', postRoutes);
 
 //lascio il server in ascolto sulla porto stabilit
 app.listen(port, () => {
--- routes/posts.js
+++ routes/posts.js
@@ -4,8 +4,8 @@ const router = express.Router();
 //definisco la route principale
 
 router.get("/", (req, res) => {
-    res.send("Lista dei post")
+    res.send("Lista dei miei post")
 });
 
 //esporto le route
-module.exports = routers;
+module.exports = router;
