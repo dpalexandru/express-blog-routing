43c3143b72993a7d8721f17d155cdaec147ed2fa
dpalexandru <alexpoian91@hotmail.com>
2025-08-01 16:30:15 +0200

    feat: add imgs an use express.static

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const app = express();
 const port = 3000;
 
 //importo le routes 
-const postRoutes = require('./routes/posts.js')
+const postRoutes = require('./routers/posts.js')
 
-//creo la rotts principale 
+//creo la routs principale 
 app.get("/", (req, res) => {
     res.send("Benvenuto, collegamento riuscito")
 });
 
 //autorizzo all'uso delle routes a posts
 app.use('/posts', postRoutes);
+
+//middlware
+app.use(express.static('public'));
 
 //lascio il server in ascolto sulla porto stabilit
 app.listen(port, () => {
